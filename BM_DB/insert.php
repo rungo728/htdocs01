d9588b7b7cb73e31e0309162e72b41ef41651ddd
rungo728 <rufii72865@gmail.com>
2020-05-22 13:46:16 +0900

    create bookmark db first commit
    ブックマークリストの作成をDBでするためにファイルを入れました。

---
+++ BM_DB/insert.php
@@ -0,0 +1,39 @@
+<?php
+//1. POSTデータ取得
+
+//まず前のphpからデーターを受け取る（この受け取ったデータをもとにbindValueと結びつけるため）
+$name = $_POST["name"];
+$email = $_POST["email"];//email
+$naiyou = $_POST["naiyou"];//naiyou
+
+//2. DB接続します xxxにDB名を入力する
+//ここから作成したDBに接続をしてデータを登録します xxxxに作成したデータベース名を書きます
+try {
+  //dbname=a_dbと記述
+  $pdo = new PDO('mysql:dbname=a_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成 //ここにカラム名を入力する
+//xxx_table(テーブル名)はテーブル名を入力します
+$stmt = $pdo->prepare("INSERT INTO a_table(id, name, email, naiyou,
+indate )VALUES(NULL, :name, :email, :naiyou, sysdate())");
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':naiyou', $naiyou, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト 書くときにLocation: in この:　のあとは半角スペースがいるので注意！！
+  header("Location: select１.php");
+  exit;
+
+}
+?>
---
+++ BM_DB/post/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="next.php" method="post">
+    <!-- inputタグをこの下から記入する -->
+    <!-- 入力データに名前をつける -->
+    <input type="text" name="name">
+    <textarea name="bb" id="" cols="30" rows="10"></textarea>
+    <button type="submit">送信</button>
+  </form>
+  
+</body>
+</html>
